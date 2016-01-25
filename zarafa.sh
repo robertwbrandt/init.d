d23bbb4978dafe863ed81220f68ae4daf9e7ab6b
Bob Brandt <bob@brandt.ie>
2016-01-25 10:22:27 +0000

    Work1

--- zarafa.sh
+++ zarafa.sh
@@ -36,15 +36,15 @@ if [ ! -r "$_this_conf" ]; then
       echo -e "    ZarafaICal=/etc/init.d/zarafa-ical"
       echo -e " ZarafaGateway=/etc/init.d/zarafa-gateway"
       echo -e "       Postfix=/etc/init.d/postfix"
-      echo -e "         MySQL=/etc/init.d/postfix"
       echo -e "        Apache=/etc/init.d/apache2"
+      echo -e "         MySQL=/etc/init.d/mysql"
+      echo -e " MySQLCredFile=/etc/mysql/debian.cnf"
       echo -e "ZarafaTestUser='castrof'"
       echo -e "ZarafaTestPass='cubacuba'" ) > "$_this_conf"
     echo "Unable to find required file: $_this_conf" 1>&2
 fi
 . "$_brandt_utils"
 . "$_this_conf"
-
 
 function setup() {
     local _status=0
@@ -55,287 +55,188 @@ function setup() {
     _status=$(( $_status | $? ))
 
     chownmod root:root 644 "$_this_conf" > /dev/null 2>&1
-    _status=$(( $_status | $? ))
     return $(( $_status | $? ))
 }
 
-function start() {
-    local _status=0    
-    echo "Starting up Zarafa Deamons"
-    if [ -z "$ZarafaServer" ]; then
-        echo -n "Starting Zarafa Server Deamon "
-        $ZarafaServer start > /dev/null 2>&1
-        brandt_status start
-        _status=$(( $_status | $? ))
+runCommand() {
+    initd="$1"
+    cmd="$2"
+    string="$3"
+    if [ -z "$initd" ]; then
+        brandt_deamon_wrapper "$string" "$initd" "$cmd"
+        return $?
     fi
-
-    if [ -z "$ZarafaDAgent" ]; then
-        echo -n "Starting Zarafa DAgent Deamon "
-        $ZarafaDAgent start > /dev/null 2>&1
-        brandt_status start
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaLicensed" ]; then
-        echo -n "Starting Zarafa License Deamon "
-        $ZarafaLicensed start > /dev/null 2>&1
-        brandt_status start
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaMonitor" ]; then
-        echo -n "Starting Zarafa Monitor Deamon "
-        $ZarafaMonitor start > /dev/null 2>&1
-        brandt_status start
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaSearch" ]; then
-        echo -n "Starting Zarafa Search Deamon "
-        $ZarafaSearch start > /dev/null 2>&1
-        brandt_status start
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaSpooler" ]; then
-        echo -n "Starting Zarafa Spooler Deamon "
-        $ZarafaSpooler start > /dev/null 2>&1
-        brandt_status start
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaPresence" ]; then
-        echo -n "Starting Zarafa Presence Deamon "
-        $ZarafaPresence start > /dev/null 2>&1
-        brandt_status start
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaICal" ]; then
-        echo -n "Starting Zarafa iCal Deamon "
-        $ZarafaICal start > /dev/null 2>&1
-        brandt_status start
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaGateway" ]; then
-        echo -n "Starting Zarafa Gateway (IMAP/POP) Deamon "
-        $ZarafaGateway start > /dev/null 2>&1
-        brandt_status start
-        _status=$(( $_status | $? ))
-    fi
-
-    return $_status
-}
-
-function stop() {
-    local _status=0    
-    echo "Shutting down Zarafa Deamons"
-    if [ -z "$ZarafaICal" ]; then
-        echo -n "Stopping Zarafa iCal Deamon "
-        $ZarafaICal stop > /dev/null 2>&1
-        brandt_status stop
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaGateway" ]; then
-        echo -n "Stopping Zarafa Gateway (IMAP/POP) Deamon "
-        $ZarafaGateway stop > /dev/null 2>&1
-        brandt_status stop
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaDAgent" ]; then
-        echo -n "Stopping Zarafa DAgent Deamon "
-        $ZarafaDAgent stop > /dev/null 2>&1
-        brandt_status stop
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaMonitor" ]; then
-        echo -n "Stopping Zarafa Monitor Deamon "
-        $ZarafaMonitor stop > /dev/null 2>&1
-        brandt_status stop
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaSearch" ]; then
-        echo -n "Stopping Zarafa Search Deamon "
-        $ZarafaSearch stop > /dev/null 2>&1
-        brandt_status stop
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaSpooler" ]; then
-        echo -n "Stopping Zarafa Spooler Deamon "
-        $ZarafaSpooler stop > /dev/null 2>&1
-        brandt_status stop
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaPresence" ]; then
-        echo -n "Stopping Zarafa Presence Deamon "
-        $ZarafaPresence stop > /dev/null 2>&1
-        brandt_status stop
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaServer" ]; then
-        echo -n "Stopping Zarafa Server Deamon "
-        $ZarafaServer stop > /dev/null 2>&1
-        brandt_status stop
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaLicensed" ]; then
-        echo -n "Stopping Zarafa License Deamon "
-        $ZarafaLicensed stop > /dev/null 2>&1
-        brandt_status stop
-        _status=$(( $_status | $? ))
-    fi
-
-    return $_status
+    return 0    
 }
 
 function checkSMTPServer() {
     IP=${1:-'127.0.0.1'}
-    echo -n "Verifying SMTP Service on $IP is responding"
-    tmp=$( ( sleep 1 ; echo "helo client.test.com"; sleep 1; echo "noop" ; sleep 1 ; echo "quit" ) | telnet "$IP" 25 2> /dev/null )
-    echo "$tmp" | grep -i "250.*OK" > /dev/null 2>&1
-    brandt_status status
-    return $?
-}
-
-function checkZarafaWebApp() {
-    IP=${1:-'127.0.0.1'}
-    echo -n "Verifying HTTP WebApp Service on $IP is responding"
-    wget --no-check-certificate -O - -o /dev/null "https://$IP/webapp" | grep '<title>Zarafa WebApp</title>' > /dev/null 2>&1
-    brandt_status status
-    return $?
+    if [ -z "$IP" ] && [ -z "$Postfix" ]; then    
+        echo -n "Verifying SMTP Service on $IP is responding"
+        tmp=$( ( sleep 1 ; echo "helo client.test.com"; sleep 1; echo "noop" ; sleep 1 ; echo "quit" ) | telnet "$IP" 25 2> /dev/null )
+        echo "$tmp" | grep -i "250.*OK" > /dev/null 2>&1
+        brandt_status status
+        return $?
+    fi
+    return 0
 }
 
 function checkIMAPServer() {
     IP=${1:-'127.0.0.1'}
-    echo -n "Verifying IMAP Service on $IP is responding"
-    tmp=$( ( sleep 1 ; echo -e "a1 login \"$ZarafaTestUser\" \"$ZarafaTestPass\""; sleep 1; echo 'a2 list "" "*"' ; sleep 1 ; echo 'a3 logout' ) | openssl s_client -connect $IP:993 -quiet 2> /dev/null )
-    ( echo "$tmp" | grep -i "a1.*OK.*LOGIN.*completed" && echo "$tmp" | grep -i "a2.*OK.*LIST.*completed" ) > /dev/null 2>&1
-    brandt_status status
-    return $?
+    if [ -z "$IP" ] && [ -z "$ZarafaGateway" ] && [ -z "$ZarafaTestUser" ] && [ -z "$ZarafaTestPass" ]; then    
+        echo -n "Verifying IMAP Service on $IP is responding"
+        tmp=$( ( sleep 1 ; echo -e "a1 login \"$ZarafaTestUser\" \"$ZarafaTestPass\""; sleep 1; echo 'a2 list "" "*"' ; sleep 1 ; echo 'a3 logout' ) | openssl s_client -connect $IP:993 -quiet 2> /dev/null )
+        ( echo "$tmp" | grep -i "a1.*OK.*LOGIN.*completed" && echo "$tmp" | grep -i "a2.*OK.*LIST.*completed" ) > /dev/null 2>&1
+        brandt_status status
+        return $?
+    fi
+    return 0
+}
+
+function checkCalDavServer() {
+    IP=${1:-'127.0.0.1'}
+    if [ -z "$IP" ] && [ -z "$ZarafaICal" ] && [ -z "$ZarafaTestUser" ] && [ -z "$ZarafaTestPass" ]; then    
+        echo -n "Verifying CalDav Service on $IP:8443 is responding"
+        wget --no-check-certificate --user="$ZarafaTestUser" --password="$ZarafaTestPass" -O - -o /dev/null "https://$IP:8443/caldav/$ZarafaTestUser/calendar" > /dev/null 2>&1
+        brandt_status status
+        return $?
+    fi
+    return 0
+}
+
+function checkZarafaWebApp() {
+    IP=${1:-'127.0.0.1'}
+    if [ -z "$IP" ] && [ -z "$Apache" ]; then        
+        echo -n "Verifying HTTP WebApp Service on $IP is responding"
+        wget --no-check-certificate -O - -o /dev/null "https://$IP/webapp" | grep '<title>Zarafa WebApp</title>' > /dev/null 2>&1
+        brandt_status status
+        return $?
+    fi
+    return 0
 }
 
 function checkZPushActiveSync() {
     IP=${1:-'127.0.0.1'}
-    echo -n "Verifying ActiveSync Service on $IP is responding"
-    wget --user="$ZarafaZPushUser" --password="$ZarafaZPushPass" -O - -o /dev/null "https://$IP/Microsoft-Server-ActiveSync" | grep '<title>Z-Push ActiveSync</title>' > /dev/null 2>&1
-    brandt_status status
-    return $?
+    if [ -z "$IP" ] && [ -z "$Apache" ] && [ -z "$ZarafaTestUser" ] && [ -z "$ZarafaTestPass" ]; then    
+        echo -n "Verifying ActiveSync Service on $IP is responding"
+        wget --no-check-certificate --user="$ZarafaTestUser" --password="$ZarafaTestPass" -O - -o /dev/null "https://$IP/Microsoft-Server-ActiveSync" | grep '<title>Z-Push ActiveSync</title>' > /dev/null 2>&1
+        brandt_status status
+        return $?
+    fi
+    return 0
+}
+
+function checkMySQL() {
+    if [ -z "$MySQL" ] && [ -z "$MySQLCredFile" ]; then
+        echo -n "Verifying MySQL Service is responding"
+        mysql --defaults-extra-file=$MySQLCredFile -e "SHOW DATABASES;" > /dev/null 2>&1
+        brandt_status status
+        return $?
+    fi
+    return 0
+}
+
+function runCommands() {
+    local _status=0
+    cmd="$1"
+
+    runCommand "$ZarafaDAgent" "$cmd" "Zarafa DAgent Deamon"
+    _status=$(( $_status | $? ))
+
+    runCommand "$ZarafaLicensed" "$cmd" "Zarafa License Deamon"
+    _status=$(( $_status | $? ))
+
+    runCommand "$ZarafaMonitor" "$cmd" "Zarafa Monitor Deamon"
+    _status=$(( $_status | $? ))
+
+    runCommand "$ZarafaSearch" "$cmd" "Zarafa Search Deamon"
+    _status=$(( $_status | $? ))
+
+    runCommand "$ZarafaSpooler" "$cmd" "Zarafa Spooler Deamon"
+    _status=$(( $_status | $? ))
+
+    runCommand "$ZarafaPresence" "$cmd" "Zarafa Presence Deamon"
+    _status=$(( $_status | $? ))
+
+    runCommand "$ZarafaServer" "$cmd" "Zarafa Server Deamon"
+    _status=$(( $_status | $? ))
+
+    runCommand "$ZarafaICal" "$cmd" "Zarafa iCal (CalDav) Deamon"
+    _status=$(( $_status | $? ))
+
+    runCommand "$ZarafaGateway" "$cmd" "Zarafa Gateway (IMAP/POP) Deamon"
+    _status=$(( $_status | $? ))
+
+    return $_status
 }
 
 function status() {
-    local _status=0    
-    if [ -z "$Postfix" ]; then
-        echo -n "Checking Postfix Deamon "
-        $Postfix status > /dev/null 2>&1
-        brandt_status status
-        _status=$(( $_status | $? ))
+    local _status=0
+    subcmd=$( lower ${1:-'all'} )
 
+    if [ "$subcmd" == "mta" ] || [ "$subcmd" == "all" ]; then
+        runCommand "$Postfix" status "Postfix (MTA) Deamon"
+        _status=$(( $_status | $? ))
         checkSMTPServer '127.0.0.1'
+        _status=$(( $_status | $? ))
+    fi
+
+    if [ "$subcmd" == "mda" ] || [ "$subcmd" == "all" ]; then
+        runCommand "$ZarafaDAgent" status "Zarafa DAgent Deamon"
+        _status=$(( $_status | $? ))
+
+        runCommand "$ZarafaLicensed" status "Zarafa License Deamon"
+        _status=$(( $_status | $? ))
+
+        runCommand "$ZarafaMonitor" status "Zarafa Monitor Deamon"
+        _status=$(( $_status | $? ))
+
+        runCommand "$ZarafaSearch" status "Zarafa Search Deamon"
+        _status=$(( $_status | $? ))
+
+        runCommand "$ZarafaSpooler" status "Zarafa Spooler Deamon"
+        _status=$(( $_status | $? ))
+
+        runCommand "$ZarafaPresence" status "Zarafa Presence Deamon"
+        _status=$(( $_status | $? ))
+
+        runCommand "$MySQL" status "MySQL Database Deamon"
+        _status=$(( $_status | $? ))
+        checkMySQL
+        _status=$(( $_status | $? ))
+
+        runCommand "$ZarafaServer" status "Zarafa Server Deamon"
         _status=$(( $_status | $? ))        
     fi
 
-    if [ -z "$MySQL" ]; then
-        echo -n "Checking MySQL Database Deamon "
-        $MySQL status > /dev/null 2>&1
-        brandt_status status
+    if [ "$subcmd" == "web" ] || [ "$subcmd" == "all" ]; then
+        runCommand "$Apache" status "Apache Webserver Deamon"
         _status=$(( $_status | $? ))
-    fi    
-
-    echo "Checking Zarafa Deamons"
-
-    if [ -z "$ZarafaServer" ]; then
-        echo -n "Checking Zarafa Server Deamon "
-        $ZarafaServer status > /dev/null 2>&1
-        brandt_status status
+        checkZarafaWebApp '127.0.0.1'
         _status=$(( $_status | $? ))
     fi
 
-    if [ -z "$ZarafaDAgent" ]; then
-        echo -n "Checking Zarafa DAgent Deamon "
-        $ZarafaDAgent status > /dev/null 2>&1
-        brandt_status status
+    if [ "$subcmd" == "imap" ] || [ "$subcmd" == "all" ]; then
+        runCommand "$ZarafaGateway" status "Zarafa Gateway (IMAP/POP) Deamon"
         _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaLicensed" ]; then
-        echo -n "Checking Zarafa License Deamon "
-        $ZarafaLicensed status > /dev/null 2>&1
-        brandt_status status
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaMonitor" ]; then
-        echo -n "Checking Zarafa Monitor Deamon "
-        $ZarafaMonitor status > /dev/null 2>&1
-        brandt_status status
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaSearch" ]; then
-        echo -n "Checking Zarafa Search Deamon "
-        $ZarafaSearch status > /dev/null 2>&1
-        brandt_status status
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaSpooler" ]; then
-        echo -n "Checking Zarafa Spooler Deamon "
-        $ZarafaSpooler status > /dev/null 2>&1
-        brandt_status status
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaPresence" ]; then
-        echo -n "Checking Zarafa Presence Deamon "
-        $ZarafaPresence status > /dev/null 2>&1
-        brandt_status status
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaICal" ]; then
-        echo -n "Checking Zarafa iCal Deamon "
-        $ZarafaICal status > /dev/null 2>&1
-        brandt_status status
-        _status=$(( $_status | $? ))
-    fi
-
-    if [ -z "$ZarafaGateway" ]; then
-        echo -n "Checking Zarafa Gateway (IMAP/POP) Deamon "
-        $ZarafaGateway status > /dev/null 2>&1
-        brandt_status status
-        _status=$(( $_status | $? ))
-
         checkIMAPServer '127.0.0.1'
-        _status=$(( $_status | $? ))        
+        _status=$(( $_status | $? ))
     fi
 
-    if [ -z "$Apache" ]; then
-        echo -n "Checking Apache Webserver Deamon "
-        $Apache status > /dev/null 2>&1
-        brandt_status status
+    if [ "$subcmd" == "caldav" ] || [ "$subcmd" == "all" ]; then
+        runCommand "$ZarafaICal" status "Zarafa iCal (CalDav) Deamon"
         _status=$(( $_status | $? ))
+        checkCalDavServer '127.0.0.1'     
+        _status=$(( $_status | $? ))
+    fi
 
-        tmp=$( find -L /etc/apache2/sites-enabled/ -type f -exec grep "Alias /webapp " "{}" \; )
-        if [ -z "$tmp" ]; then
-            checkZarafaWebApp '127.0.0.1'
-            _status=$(( $_status | $? ))
-        fi
+    if [ "$subcmd" == "mobile" ] || [ "$subcmd" == "all" ]; then
+        runCommand "$Apache" status "Apache Webserver Deamon"
+        _status=$(( $_status | $? ))
+        checkZPushActiveSync '127.0.0.1'
+        _status=$(( $_status | $? ))
+    fi
 
-        tmp=$( find -L /etc/apache2/sites-enabled/ -type f -exec grep "/Microsoft-Server-ActiveSync" "{}" \; )
-        if [ -z "$tmp" ]; then
-            checkZPushActiveSync '127.0.0.1'
-            _status=$(( $_status | $? ))
-        fi
-    fi   
     return $_status
 }
 
@@ -345,7 +246,9 @@ function usage() {
     [ "$_exitcode" == "0" ] && _output=1
     [ "$2" == "" ] || echo -e "$2"
     ( echo -e "Usage: $0 [options] command"
-      echo -e "Commands:  start     stop     status     restart"
+      echo -e "Commands:  start     stop     restart"
+      echo -e "           reload    force-reload"
+      echo -e "           status [all|mda|mta|web|imap|caldav|mobile]"
       echo -e "Options:"
       echo -e " -h, --help     display this help and exit"
       echo -e " -v, --version  output version information and exit" ) >&$_output
@@ -375,14 +278,13 @@ _command=$( lower "$1" )
 shift 1
 
 # Check to see if user is root, if not re-run script as root.
-brandt_amiroot || { echo "${BOLD_RED}This program must be run as root!${NORMAL}" >&2 ; sudo "$0" $@ ; exit $?; }
+brandt_amiroot || { echo "${BOLD_RED}This program must be run as root!${NORMAL}" >&2 ; sudo "$0" "$_command" $@ ; exit $?; }
 
 case "$_command" in
-    "start" )               start ;;
-    "stop" )                stop ;;
-    "status" )              status $@ ;;
-    "restart"|"reload")     stop ; start ;;
-    "setup" )               setup ;; 
-    * )                     usage 1 ;;
+    "status" )  status $@ ;;
+    "start"|"stop"|"restart"|"reload"|"force-reload")
+                runCommands "$_command" $@ ;;
+    "setup" )   setup ;; 
+    * )         usage 1 ;;
 esac
 exit $?
